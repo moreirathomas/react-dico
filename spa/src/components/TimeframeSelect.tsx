53dfe4569e6c4e8555c56e47ff722d985ec45eb8
moreirathomas <moreirathomas97@gmail.com>
2021-05-28 15:27:55 +0200

    refactor: :recycle: timestamp generation and onclick call

--- spa/src/components/TimeframeSelect.tsx
+++ spa/src/components/TimeframeSelect.tsx
@@ -6,17 +6,23 @@ interface Props {
 
 const ONE_MONTH_UNIX = 1000 * 60 * 60 * 24 * 30;
 
+/**
+ * @returns Unix timestamp with seconds precision for a given bumber of month in the past.
+ */
+function getOlderMonthTimestamp(months: number): number {
+  // Normalize the timestamp by removing the milliseconds after computation.
+  return (Date.now() - ONE_MONTH_UNIX * months) / 1000;
+}
+
 export const TimeframeSelect: React.FunctionComponent<Props> = ({ setter }) => {
-  const getPreviousMonthTimestamp = (months: number) =>
-    // Normalize the timestamp by removing the milliseconds after computation.
-    (Date.now() - ONE_MONTH_UNIX * months) / 1000;
+  const setFilterByMonth = (nb: number) => () => setter(getOlderMonthTimestamp(nb));
 
   return (
     <div>
       <p>Filter</p>
-      <button onClick={() => setter(getPreviousMonthTimestamp(1))}>One month</button>
-      <button onClick={() => setter(getPreviousMonthTimestamp(6))}>Six months</button>
-      <button onClick={() => setter(getPreviousMonthTimestamp(24))}>One year</button>
+      <button onClick={setFilterByMonth(1)}>One month</button>
+      <button onClick={setFilterByMonth(6)}>Six months</button>
+      <button onClick={setFilterByMonth(2)}>One year</button>
     </div>
   );
 };
